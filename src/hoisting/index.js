4d3f2f2a0afb2be1fd5bd64b4087f00f63448581
IsraelR20 <israelrojasvilca@gmail.com>
2023-06-01 13:35:51 -0500

    hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,9 @@
+// var nameOfDog; //se hace hoisting
+// console.log(nameOfDog);
+
+// var nameOfDog = "Elmo";
+// console.log(nameOfDog);
+nameOfDog();
+function nameOfDog(){
+    console.log(`El mejor perrito es ${elmo}`);
+}
